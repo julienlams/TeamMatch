8e368673d11f1e4a756b3e10952ea4b563843af9
Arnaud Lothe <arnaud.lothe@gmail.com>
2024-07-06 15:42:15 +0200

    box de chat terminée

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -2,13 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number }
   static targets = ["messages"]
+
 
   connect() {
     this.subscription = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this._insertMessageAndScrollDown(data) } // Changed to underscore
+      { received: data => this.#insertMessageAndScrollDown(data) } // Changed to underscore
     )
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
   }
@@ -22,8 +23,26 @@ export default class extends Controller {
     event.target.reset()
   }
 
-  _insertMessageAndScrollDown(data) { // Changed to underscore
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  #insertMessageAndScrollDown(data) { // Changed to underscore
+    const messageHTML = this.#buildMessageElement(this.currentUserIdValue === data.sender_id, data.message);
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageHTML);
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight);
+    console.log("Scroll Height before scrollTo:", this.messagesTarget.scrollHeight);
+  }
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
   }
 }
