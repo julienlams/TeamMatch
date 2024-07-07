cb1008655a383d3c5e1f0a3829db663d73e99660
Stephanie ametepe <stephyanelone@gmail.com>
2024-07-07 20:09:45 +0200

    homepage design

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class CompetitionController < ApplicationController
     before_action :set_competition, only: [:show, :edit, :update, :destroy]
     skip_before_action :authenticate_user!, only: :index
@@ -61,35 +60,4 @@ class CompetitionController < ApplicationController
     end
   end
 
-=======
-class CompetitionsController < ApplicationController
-  def index
-    @competitions = Competition.all
-  end
-
-  def new
-    @competition = Competition.new
-    @sports = Sport.all  # Pour remplir le menu déroulant des sports
-  end
-
-  def create
-    @competition = Competition.new(competition_params)
-    if @competition.save
-      redirect_to @competition, notice: 'Competition was successfully created.'
-    else
-      @sports = Sport.all
-      render :new
-    end
-  end
-
-  def show
-    @competition = Competition.find(params[:id])
-  end
-
-  private
-
-  def competition_params
-    params.require(:competition).permit(:sport_id, :number_of_teams, :date_time, :address, :name)
-  end
->>>>>>> master
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2024_07_05_110142) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2024_07_06_143409) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,11 +31,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_06_143409) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-<<<<<<< HEAD
     t.text "description"
     t.bigint "user_id"
-=======
->>>>>>> master
     t.index ["sport_id"], name: "index_competitions_on_sport_id"
   end
 
