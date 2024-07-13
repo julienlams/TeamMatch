1835c3a80f68ee48feafb2671ad40a1fb05f63ef
Arnaud Lothe <arnaud.lothe@gmail.com>
2024-07-13 23:47:19 +0200

    Fin du back messages, reste à rendre joli le front

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,8 @@ class Chatroom < ApplicationRecord
   belongs_to :team
   has_many :users, through: :team
   has_many :messages, dependent: :destroy
+
+  def last_message
+    messages.order(created_at: :desc).first
+  end
 end
