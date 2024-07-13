79be8bd19f302169cedf2cb8a70dd08cc5164427
Arnaud Lothe <arnaud.lothe@gmail.com>
2024-07-13 16:13:08 +0200

    affichage des teams avec un message dans la page MESSAGES

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :competitions, only: [:new, :create, :show, :index]
-  resources :chatrooms, only: :show do
+
+  resources :chatrooms, only: [:show, :index] do
     resources :messages, only: :create
   end
 
