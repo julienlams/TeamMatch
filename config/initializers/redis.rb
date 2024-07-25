2cb78a3f20ab61a476416acfcfa5253dfc4c268c
Julien Lamouroux <lamourouxjulien+lewagon@icloud.com>
2024-07-25 23:41:30 +0200

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
-Redis.current = Redis.new(
+require 'redis'
+
+$redis = Redis.new(
   url: 'rediss://ec2-63-34-5-2.eu-west-1.compute.amazonaws.com:29599',
   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_PEER, ca_file: '/path/to/ca_certificate.pem' }
 )
