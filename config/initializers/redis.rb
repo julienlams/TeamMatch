05f5ecfae018a08bdb11c1a0dff3ca0b042d5e10
Kilanga <34622052+Kilanga@users.noreply.github.com>
2024-07-26 00:20:20 +0200

    Revert "redis"

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,8 @@
 require 'redis'
 
-redis_url = ENV["REDIS_URL"] || "rediss://ec2-63-34-5-2.eu-west-1.compute.amazonaws.com:29599"
+redis_url = ENV["REDIS_URL"] || "rediss://:pafd83a37af5b6215b24e2b215e2100a8fb87f2d6f24cf2c17fd12e222291780d@ec2-63-34-5-2.eu-west-1.compute.amazonaws.com:29599"
 
 $redis = Redis.new(
   url: redis_url,
-  ssl_params: {
-    verify_mode: OpenSSL::SSL::VERIFY_PEER,
-    ca_file: Rails.root.join('config/ca_certificate.pem').to_s
-  }
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_PEER }
 )
