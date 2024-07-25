b3fbb986d098544a1e2ea4b6be4a040e5a293aab
Kilanga <34622052+Kilanga@users.noreply.github.com>
2024-07-26 00:21:30 +0200

    Revert "test redis"

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,6 @@
 require 'redis'
 
-redis_url = ENV["REDIS_URL"] || "rediss://:pafd83a37af5b6215b24e2b215e2100a8fb87f2d6f24cf2c17fd12e222291780d@ec2-63-34-5-2.eu-west-1.compute.amazonaws.com:29599"
-
 $redis = Redis.new(
-  url: redis_url,
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_PEER }
+  url: 'rediss://ec2-63-34-5-2.eu-west-1.compute.amazonaws.com:29599',
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
 )
