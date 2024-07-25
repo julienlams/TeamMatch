f399e61b39e411b3a03a03f35ad92672749d2374
Julien Lamouroux <lamourouxjulien+lewagon@icloud.com>
2024-07-25 23:47:29 +0200

    no ssl redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,5 @@ require 'redis'
 
 $redis = Redis.new(
   url: 'rediss://ec2-63-34-5-2.eu-west-1.compute.amazonaws.com:29599',
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_PEER, ca_file: '/path/to/ca_certificate.pem' }
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
 )
